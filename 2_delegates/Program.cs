404f88b51479f2fc1e3043e1553960af550b1b41
Matvey Vizir <112975341+MaTTveIg@users.noreply.github.com>
2023-09-06 12:47:30 +0200

    Add files via upload

---
+++ 2_delegates/Program.cs
@@ -0,0 +1,18 @@
+﻿EmailValidator emailValidator = new();
+
+emailValidator.ValidEmail("artem@gmail.com", 
+    email => Console.WriteLine($"{email} is valid"),
+    email => Console.WriteLine($"{email} is invalid"),
+    email => email.Contains('@'));
+
+class EmailValidator
+{
+    public delegate void Callback(string email);
+    public delegate bool ValidateHandler(string email);
+    public void ValidEmail(string email, Callback validEmail, Callback invalidEmail, ValidateHandler validateHandler)
+    {
+        if (validateHandler(email))
+            validEmail(email);
+        else invalidEmail(email);
+    }
+}
